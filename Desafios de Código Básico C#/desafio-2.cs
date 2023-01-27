fa75500e233b7e2151ce60384efdb714bd951e04
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-01-27 14:33:50 -0300

    Primeiros desafios do bootcamp c#

---
+++ Desafios de Código Básico C#/desafio-2.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using System.Linq;
+using System;
+
+class Program {
+  static void Main(string[] args) {
+    int qt = int.Parse(Console.ReadLine());
+    string[] v = new string[2];
+    
+    for (int i = 0; i < qt; ++i) {
+      v = Console.ReadLine().Split(" ");
+      string a = v[0];
+      string b = v[1];
+
+      if (b.Length > a.Length)
+        Console.WriteLine("nao encaixa");
+      else if (a.EndsWith(b))
+        Console.WriteLine("encaixa");
+      else
+        Console.WriteLine("nao encaixa");
+    }
+  }
+}
